17b10249ed008db510507d78847db932bb821c75
Jose <jgonzalezmn@gmail.com>
2019-06-17 14:17:14 -0400

    social card done

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+import CardCointainer from './CardContainer'
+
+function Banner () {
+    return (
+        <div>
+            <a href="https://www.reactjs.org"> 
+          <CardCointainer />
+          </a>
+        </div>
+    )
+}
+
+export default Banner
